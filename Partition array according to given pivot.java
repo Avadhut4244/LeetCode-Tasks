44e0f58625e63c132eb7d01874210f00bedeab50
Avadhut4244 <160886277+Avadhut4244@users.noreply.github.com>
2025-03-03 23:41:26 +0530

    Create Partition array according to given pivot.java

---
+++ Partition array according to given pivot.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        List<Integer> less = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
+        
+        // Categorizing elements
+        for (int num : nums) {
+            if (num < pivot) {
+                less.add(num);
+            } else if (num > pivot) {
+                greater.add(num);
+            } else {
+                equal.add(num);
+            }
+        }
+        
+        // Merging lists into result array
+        int index = 0;
+        for (int num : less) nums[index++] = num;
+        for (int num : equal) nums[index++] = num;
+        for (int num : greater) nums[index++] = num;
+        
+        return nums;
+    }
+}
